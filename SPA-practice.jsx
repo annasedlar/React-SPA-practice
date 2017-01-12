b77506e5aa7351b7c6cc2919b20f63364fa8a9aa
Anna Sedlar <annasedlar@gmail.com>
2017-01-12 10:17:10 -0500

    add styles, nest basic children

--- SPA-practice.jsx
+++ SPA-practice.jsx
@@ -1,15 +1,62 @@
+var Contact = React.createClass({
+  render: function() {
+      return (
+        <div>
+          <h2>GOT QUESTIONS?</h2>
+          <p>The easiest thing to do is post on
+          our <a href="http://forum.kirupa.com">forums</a>.
+          </p>
+        </div>
+      );
+    }
+});
+ 
+var Stuff = React.createClass({
+  render: function() {
+      return (
+        <div>
+          <h2>STUFF</h2>
+          <p>Mauris sem velit, vehicula eget sodales vitae,
+          rhoncus eget sapien:</p>
+          <ol>
+            <li>Nulla pulvinar diam</li>
+            <li>Facilisis bibendum</li>
+            <li>Vestibulum vulputate</li>
+            <li>Eget erat</li>
+            <li>Id porttitor</li>
+          </ol>
+        </div>
+      );
+    }
+});
+
+function Home(props){
+	return(
+		<div>
+			<h1>This is the home page: Welcome, {props.name}</h1>
+			<p>Cras facilisis urna ornare ex volutpat, et
+	          convallis erat elementum. Ut aliquam, ipsum vitae
+	          gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+	          metus nec massa. Maecenas hendrerit laoreet augue
+	          nec molestie. Cum sociis natoque penatibus et magnis
+	          dis parturient montes, nascetur ridiculus mus.
+	        </p>
+		</div>
+	)
+}
+
 var App = React.createClass({
 	render: function(){
 		return(
 		<div>
         	<h1>Simple SPA</h1>
 	        <ul className="header">
-	          <li>Home</li>
-	          <li>Stuff</li>
-	          <li>Contact</li>
+          <li><ReactRouter.Link to="/">Home</ReactRouter.Link></li>
+          <li><ReactRouter.Link to="/stuff">Stuff</ReactRouter.Link></li>
+          <li><ReactRouter.Link to="/contact">Contact</ReactRouter.Link></li>
 	        </ul>
         <div className="content">
- 
+ 			{this.props.children}
         </div>
       </div>
     )
@@ -19,6 +66,9 @@ var App = React.createClass({
 ReactDOM.render(
 	<ReactRouter.Router> 
 		<ReactRouter.Route path="/" component= {App}>
+			<ReactRouter.IndexRoute component={Home} />
+			<ReactRouter.Route path="stuff" component={Stuff} />
+     		<ReactRouter.Route path="contact" component={Contact} />
 		</ReactRouter.Route>
 	</ReactRouter.Router>,
 	document.getElementById("container")
